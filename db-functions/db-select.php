1c3adf595b517d00149a525168ff57488e75b040
Rudy538 <leszczyk.of@gmail.com>
2023-12-10 21:37:44 +0100

    Update db-select.php

--- db-functions/db-select.php
+++ db-functions/db-select.php
@@ -8,22 +8,29 @@ $pass = $_POST["pass"];
 $query = "SELECT email, pass FROM users WHERE email = '$email' AND pass = '$pass'";
 $result = mysqli_query($conn, $query);
 
-mysqli_num_rows($result);
+$num_rows = mysqli_num_rows($result);
 $row = mysqli_fetch_array($result);
 
-$db_email = $row["email"];
-$db_pass = $row["pass"];
+if ($num_rows > 0) {
+  $_SESSION["db_email"] = $row["email"];
+  $_SESSION["db_pass"] = $row["pass"];
+} else {
+  session_unset();
+  PhpAlert("Wrong email or password");
+  exit();
+}
 
 if (isset($_SESSION["email"])) {
   PhpAlert('You are already logged in!');
-} elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  if ($pass = $db_pass && $email = $db_email) {
-    PhpAlert('Logged in as: ' . $email);
+}
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  if ($pass = $_SESSION["db_pass"] && $email = $_SESSION["db_email"]) {
+    PhpAlert('Logged in as: ' . $_SESSION["db_email"]);
   } else {
     PhpAlert('Wrong email or password');
   }
 } else {
-  header("Location: ./views/index.php");
+  header("Location: ../views/index.php");
 }
 
-$_SESSION["email"] = "$email";
+$_SESSION["email"] = $_SESSION["db_email"];
