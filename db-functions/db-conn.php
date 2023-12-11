b38e68e13d3feb63a41e4e12937c847efb9f462d
Rudy538 <leszczyk.of@gmail.com>
2023-12-11 13:23:34 +0100

    Update db-conn.php

--- db-functions/db-conn.php
+++ db-functions/db-conn.php
@@ -1,16 +1,15 @@
 <?php
 
-
 $conn = mysqli_connect("localhost", "root", "", "TokyoThreads");
 
 if (!$conn) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    exit();
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit();
 }
 
 function PhpAlert($message)
 {
-    echo <<<END
+  echo <<<END
     <script>
     alert('$message');
     document.location.href = '../views/index.php';
