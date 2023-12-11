5b27d4c604124205e9b49a7d829612caa83f27d4
Rudy538 <leszczyk.of@gmail.com>
2023-12-11 13:45:24 +0100

    Update register.php

--- views/register.php
+++ views/register.php
@@ -18,7 +18,7 @@ session_start();
 <body>
     <div class="dark-bg"></div>
     <div class="main">
-        <div class="header">
+    <div class="header">
             <script type="text/javascript" src="../Js/dropdown/dropdown.js"></script>
             <div class="dropdown-lang">
                 <button class="dropdown-lang-bt" onclick="dropdown('dropdown-lang-content')"><img class="dropdown-lang-img" src="../icons/earth.png"></button>
@@ -38,10 +38,10 @@ session_start();
                 <div class="dropdown-login">
                     <button class="dropdown-login-bt" onclick="dropdown('dropdown-login-content')">
                         <?php
-                        if (isset($_SESSION["email"])) {
-                            echo '<img class="dropdown-login-img" src="../icons/user-logged.png" alt="user-logged" style="margin-top: 2px; width: 25px;">';
+                        if (isset($_SESSION["email"]) && isset($_SESSION["pass"])) {
+                            echo '<img class="dropdown-login-img" src="../icons/user-logged.png" alt="user-logged" style="width: 25px;">';
                         } else {
-                            echo '<img class="dropdown-login-img"  src="../icons/user.png" alt="user-not-logged" style="margin-top: 1px; width: 25px;">';
+                            echo '<img class="dropdown-login-img"  src="../icons/user.png" alt="user-not-logged" style="width: 25px;">';
                         }
                         ?>
                     </button>
@@ -49,15 +49,15 @@ session_start();
                         <a href="./login.php">Log In</a>
                         <a href="./register.php">Sign Up</a>
                         <?php
-                        if (isset($_SESSION["email"])) {
-                            echo '<a href="../views/account.php">Account</a>';
+                        if (isset($_SESSION["email"]) && isset($_SESSION["pass"])) {
+                            echo '<a href="./account.php">Account</a>';
                         } else {
                             echo '';
                         }
                         ?>
                     </div>
                 </div>
-                <button><img src="../icons/cart.png"></button>
+                <a href="./cart.php"><button class="cart"><img src="../icons/cart.png"></button></a>
             </div>
 
             <div class="nav">
